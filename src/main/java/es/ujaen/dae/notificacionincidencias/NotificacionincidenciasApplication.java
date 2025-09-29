62a3d2348e6be8e3073499dfb2add5f73f6115b3
Carlos Martínez Sánchez <cms00065@red.ujaen.es>
2025-09-29 20:45:10 +0200

    Iniciado el proyecto

---
+++ src/main/java/es/ujaen/dae/notificacionincidencias/NotificacionincidenciasApplication.java
@@ -0,0 +1,13 @@
+package es.ujaen.dae.notificacionincidencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificacionincidenciasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificacionincidenciasApplication.class, args);
+    }
+
+}
