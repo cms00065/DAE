ea18cca20336ad37cc07a90ba73d52549042e122
Carlos Martínez Sánchez <cms00065@red.ujaen.es>
2025-10-27 20:49:01 +0100

    Modificado el archivo "NotificacionIncidencias.java" para añadir la anotación "@EntityScan"

--- src/main/java/es/ujaen/dae/notificacionincidencias/app/NotificacionIncidencias.java
+++ src/main/java/es/ujaen/dae/notificacionincidencias/app/NotificacionIncidencias.java
@@ -2,8 +2,10 @@ package es.ujaen.dae.notificacionincidencias.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication(scanBasePackages = "es.ujaen.dae.notificacionincidencias.servicios")
+@EntityScan(basePackages = "es.ujaen.dae.notificacionincidencias.entidades")
 public class NotificacionIncidencias {
 
     public static void main(String[] args) {
