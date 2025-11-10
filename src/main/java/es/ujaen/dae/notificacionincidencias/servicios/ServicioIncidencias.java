5b68315996d0cd6e6bd875a6f0fdf4534305d547
Javier Moreno Arjona <jma00081@red.ujaen.es>
2025-11-10 20:20:51 +0100

    "ServicioIncidencias.java" adaptado a JPA

--- src/main/java/es/ujaen/dae/notificacionincidencias/servicios/ServicioIncidencias.java
+++ src/main/java/es/ujaen/dae/notificacionincidencias/servicios/ServicioIncidencias.java
@@ -1,19 +1,18 @@
 package es.ujaen.dae.notificacionincidencias.servicios;
 
 import es.ujaen.dae.notificacionincidencias.entidades.*;
-import es.ujaen.dae.notificacionincidencias.excepciones.UsuarioNoDisponible;
-import es.ujaen.dae.notificacionincidencias.excepciones.UsuarioNoEsAdmin;
-import es.ujaen.dae.notificacionincidencias.excepciones.UsuarioNoLogueado;
+import es.ujaen.dae.notificacionincidencias.excepciones.*;
+import es.ujaen.dae.notificacionincidencias.repositorios.RepositorioIncidencia;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 /**
  * @author jma00081
@@ -21,92 +20,79 @@ import java.util.Optional;
 @Service
 @Validated
 public class ServicioIncidencias {
-    Map<Integer, TipoIncidencia> tiposIncidencia;
-    Map<Integer, Incidencia> incidenciasCreadas;
-    Map<Integer, List<Incidencia>> incidenciasPorUsuario;
-
-    EstadoIncidencia filtroEstado;
+    @Autowired
+    RepositorioIncidencia repositorioIncidencias;
 
     public ServicioIncidencias() {
-        tiposIncidencia = new HashMap<>();
-        incidenciasCreadas = new HashMap<>();
-        incidenciasPorUsuario = new HashMap<>();
-        filtroEstado = null;
+
     }
 
     /**
      * Crea una nueva incidencia
-     * @param actor El usuario que crea la incidencia
-     * @throws UsuarioNoLogueado Si el usuario no ha iniciado sesión
      * @param nuevaIncidencia La incidencia a crear
+     * @throws IncidenciaYaCreada Si la incidencia ya etsaba registrada
      */
-    public void crearIncidencia(Usuario actor, @Valid Incidencia nuevaIncidencia) {
-        TipoIncidencia tipo = nuevaIncidencia.tipo();
-        incidenciasCreadas.put(nuevaIncidencia.id(), nuevaIncidencia);
-        tiposIncidencia.put(tipo.id(), tipo);
-        incidenciasPorUsuario.computeIfAbsent(actor.id(), k -> new java.util.ArrayList<>()).add(nuevaIncidencia);
-    }
-
-    /**
-     * Lista las incidencias creadas por el usuario actor
-     * @param actor El usuario que solicita la lista
-     * @throws UsuarioNoLogueado Si el usuario no ha iniciado sesión
-     * @return La lista de incidencias creadas por el usuario
-     */
-    public List<Incidencia> listarMisInicidencias(Usuario actor) {
-        return incidenciasPorUsuario.getOrDefault(actor.id(), List.of());
+    public void crearIncidencia(@Valid Incidencia nuevaIncidencia) {
+        repositorioIncidencias.guardar(nuevaIncidencia);
     }
 
     /**
      * Busca incidencias según los filtros proporcionados
-     * @param actor El usuario que realiza la búsqueda
-     * @throws UsuarioNoLogueado Si el usuario no ha iniciado sesión
-     * @throws UsuarioNoEsAdmin Si el usuario no es ADMIN
-     * @param tipoId El id del tipo de incidencia a filtrar (0 para no filtrar por tipo)
-     * @param estado El estado de la incidencia a filtrar (null para no filtrar por estado)
+     * @param actor El usuario cuyas incidencias se quieres buscar
      * @return La lista de incidencias que cumplen los filtros
      */
-    public List<Incidencia> buscar(Usuario actor, @PositiveOrZero int tipoId, EstadoIncidencia estado) {
-        if (actor.rol() != Rol.ADMIN) {
-            throw new UsuarioNoEsAdmin();
-        }
+    public List<Incidencia> buscarIncidenciasCreadasPor(Usuario actor) {
+        return repositorioIncidencias.buscarIncidenciasCreadasPor(actor);
+    }
 
-        return incidenciasPorUsuario.getOrDefault(actor.id(), List.of()).stream()
-                .filter(incidencia -> (tipoId == 0 || incidencia.tipo().id() == tipoId) &&
-                        (estado == null || incidencia.estado() == estado))
-                .toList();
+    /**
+     * Filtra las incidencias por tipo
+     * @param tipo El tipo por el que queremos filtrar
+     * @return Lista de incidencias cuyo tipo sea el buscado
+     */
+    public List<Incidencia> buscarIncidenciasPorTipo(TipoIncidencia tipo) {
+        return repositorioIncidencias.buscarPorTipo(tipo);
+    }
+
+    /**
+     * Filtra las incidencias por estado
+     * @param estado El estado por el que queremos filtrar
+     * @return Lista de incidencias del estado buscado
+     */
+    public List<Incidencia> buscarIncidenciasPorEstado(EstadoIncidencia estado) {
+        return repositorioIncidencias.buscarPorEstado(estado);
     }
 
     /**
      * Borra una incidencia si el usuario actor tiene permiso para hacerlo
      * @param actor El usuario que solicita el borrado
-     * @throws UsuarioNoLogueado Si el usuario no ha iniciado sesión
      * @param incidencia La incidencia a borrar
      */
     public void borrar(Usuario actor, @Valid Incidencia incidencia) {
         if (incidencia != null && incidencia.puedeBorrar(actor)) {
-            incidenciasCreadas.remove(incidencia.id());
-            incidenciasPorUsuario.getOrDefault(actor.id(), List.of()).remove(incidencia);
+            repositorioIncidencias.eliminar(incidencia);
         }
     }
 
     /**
      * Cambia el estado de una incidencia
      * @param actor El usuario que solicita el cambio de estado
-     * @throws UsuarioNoLogueado Si el usuario no ha iniciado sesión
      * @throws UsuarioNoEsAdmin Si el usuario no es ADMIN
+     * @throws IncidenciaNoDisponible si la incidencia no está registrada
      * @param incidencia La incidencia a modificar
      * @param nuevoEstado El nuevo estado que se asignará a la incidencia
-     * @return La incidencia modificada, o un Optional vacío si no se encontró la incidencia
      */
-    public Optional<Incidencia> cambiarEstado(Usuario actor, @Valid Incidencia incidencia, EstadoIncidencia nuevoEstado) {
+    @Transactional
+    public void cambiarEstado(Usuario actor, @Valid Incidencia incidencia, EstadoIncidencia nuevoEstado) {
         if (actor.rol() != Rol.ADMIN) {
             throw new UsuarioNoEsAdmin();
         }
 
-        if (incidencia != null) {
-            incidencia.cambiarEstado(nuevoEstado);
+        Optional<Incidencia> incidenciaExistente = repositorioIncidencias.buscarPorId(incidencia.id());
+        if (incidenciaExistente.isEmpty()) {
+            throw new IncidenciaNoDisponible();
         }
-        return Optional.ofNullable(incidencia);
+
+        repositorioIncidencias.actualizarEstado(incidencia, nuevoEstado);
     }
 }
