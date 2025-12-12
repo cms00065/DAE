8ee5ba45f5925e209230c2d40802aa41d6d63600
Javier Moreno Arjona <jma00081@red.ujaen.es>
2025-12-12 19:23:51 +0100

    "ServicioSeguridad.java" implementado

--- src/main/java/es/ujaen/dae/notificacionincidencias/seguridad/ServicioSeguridad.java
+++ src/main/java/es/ujaen/dae/notificacionincidencias/seguridad/ServicioSeguridad.java
@@ -1,4 +1,42 @@
 package es.ujaen.dae.notificacionincidencias.seguridad;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
+
+@Configuration
 public class ServicioSeguridad {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.disable())
+                .httpBasic(httpBasic -> httpBasic.realmName("notificacionincidencias"))
+                .authorizeHttpRequests(request -> request
+                        // Solo un usuario ADMIN puede actualizar el estado de una incidencia
+                        .requestMatchers(HttpMethod.POST, "/incidencias/{id}/actualizarEstado").hasRole("ADMIN")
+
+                        // Todos los usuarios pueden crear incidencias
+                        .requestMatchers(HttpMethod.POST, "/incidencias").hasAnyRole("ADMIN", "CIUDADANO")
+
+                        .requestMatchers(HttpMethod.GET, "/incidencias/**").authenticated()
+
+                        // Solo un usuario ADMIN o el propio usuario puede ver sus datos
+                        .requestMatchers(HttpMethod.GET, "/usuarios/{id}").access(new WebExpressionAuthorizationManager("hasRole('ADMIN') or #id == principal.username"))
+
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
 }
