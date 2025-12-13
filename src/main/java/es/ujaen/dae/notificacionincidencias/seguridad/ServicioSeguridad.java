e531a577f370eb5e491cddb69c673e7c2605865f
Carlos Martínez Sánchez <cms00065@red.ujaen.es>
2025-12-13 11:54:12 +0100

    Modificación realizada en "ServicioSeguridad.java"

--- src/main/java/es/ujaen/dae/notificacionincidencias/seguridad/ServicioSeguridad.java
+++ src/main/java/es/ujaen/dae/notificacionincidencias/seguridad/ServicioSeguridad.java
@@ -3,6 +3,8 @@ package es.ujaen.dae.notificacionincidencias.seguridad;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,6 +17,11 @@ public class ServicioSeguridad {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authManager(AuthenticationConfiguration authConf) throws Exception {
+        return authConf.getAuthenticationManager();
     }
 
     @Bean
